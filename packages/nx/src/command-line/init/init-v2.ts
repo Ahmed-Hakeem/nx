f5ad2265b783cc8f1d4c6d83eb5f0b376fc86313
Emily Xiong <xiongemi@gmail.com>
2024-10-29 12:40:39 -0400

    fix(core): not exit when plugin installation failed

--- packages/nx/src/command-line/init/init-v2.ts
+++ packages/nx/src/command-line/init/init-v2.ts
@@ -1,4 +1,5 @@
 import { existsSync } from 'fs';
+import * as chalk from 'chalk';
 import { PackageJson } from '../../utils/package-json';
 import { prerelease } from 'semver';
 import { output } from '../../utils/output';
@@ -28,6 +29,7 @@ import { addNxToNpmRepo } from './implementation/add-nx-to-npm-repo';
 import { addNxToMonorepo } from './implementation/add-nx-to-monorepo';
 import { NxJsonConfiguration, readNxJson } from '../../config/nx-json';
 import { getPackageNameFromImportPath } from '../../utils/get-package-name-from-import-path';
+import { bold } from 'chalk';
 
 export interface InitArgs {
   interactive: boolean;
@@ -41,7 +43,10 @@ export function installPlugins(
   plugins: string[],
   pmc: PackageManagerCommands,
   updatePackageScripts: boolean
-) {
+): {
+  succeededPlugins: string[];
+  failedPlugins: string[];
+} {
   if (plugins.length === 0) {
     return;
   }
@@ -51,18 +56,41 @@ export function installPlugins(
   runInstall(repoRoot, pmc);
 
   output.log({ title: '🔨 Configuring plugins' });
+  const succeededPlugins = [];
+  const failedPlugins = [];
   for (const plugin of plugins) {
-    execSync(
-      `${pmc.exec} nx g ${plugin}:init --keepExistingVersions ${
-        updatePackageScripts ? '--updatePackageScripts' : ''
-      }`,
-      {
+    const command = `${pmc.exec} nx g ${plugin}:init --keepExistingVersions ${
+      updatePackageScripts ? '--updatePackageScripts' : ''
+    }`;
+    try {
+      execSync(command, {
         stdio: [0, 1, 2],
         cwd: repoRoot,
         windowsHide: false,
-      }
-    );
+      });
+      succeededPlugins.push(plugin);
+    } catch (e) {
+      failedPlugins.push(plugin);
+      output.error({
+        title: `Install plugin ${plugin} failed: ${
+          e.message || 'Unknown error'
+        }`,
+        bodyLines: [
+          `Installed ${plugin} but failed to run:`,
+          bold(command),
+          `Please fix error and rerun the command:`,
+          e.stack,
+        ].filter(Boolean),
+      });
+    }
   }
+  if (succeededPlugins.length > 0) {
+    output.success({
+      title: 'Installed Plugins',
+      bodyLines: succeededPlugins.map((p) => `- ${p}`),
+    });
+  }
+  return { succeededPlugins, failedPlugins };
 }
 
 export async function initHandler(options: InitArgs): Promise<void> {
